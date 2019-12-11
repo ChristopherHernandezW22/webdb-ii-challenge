2a68fbb5888d26c3b1e3485bd2980acf4777cfcd
Christopher Hernandez <cerberean661@gmail.com>
2019-12-11 15:22:59 -0800

    first commit

---
+++ data/migrations/20191211000341_cars.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+  return (
+      knex.schema.createTable('cars', table => {
+          table.increments();
+          table.string('VIN');
+          table.string('make');
+          table.string('model');
+          table.integer('mileage');
+          table.string('transmission');
+          table.string('status')
+            .nullable();
+      })
+  )
+};
+
+exports.down = function(knex) {
+  return (
+      knex.schema.dropTableIfExists('cars')
+  )
+};
